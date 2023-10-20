610789955679e38393d8ddf538c12be489855d83
Isaac Santiago <eoisaacc@gmail.com>
2023-10-19 21:40:53 -0300

    build: install and configure tailwindcss and shadcn ui

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,3 +1,13 @@
+import { Button } from '@/components/ui/button'
+import '@/styles/globals.css'
+
 export const App = () => {
-  return <h1>SecureGate</h1>
+  const handleClick = () => console.log('clicked')
+
+  return (
+    <h1 className="bg-orange-400">
+      SecureGate
+      <Button onClick={handleClick}>Click me</Button>
+    </h1>
+  )
 }
