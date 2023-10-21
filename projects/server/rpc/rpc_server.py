1ede384669fe59be7bb4220358454fbf10f3ad28
PyMarcus <marcus-v@outlook.com>
2023-10-21 16:08:29 -0300

    :construction: add rpc server description

--- projects/server/rpc/rpc_server.py
+++ projects/server/rpc/rpc_server.py
@@ -5,7 +5,30 @@ from libs import LogMaker
 from rpc_server_interface import RPCServerInterface
 
 
+@Pyro4.expose
 class RPCServer(RPCServerInterface):
+    """
+    The SecureGate RPC server offers seamless integration of distributed databases,
+    front-end applications, and MQTT messaging, providing a robust solution for interconnected systems.
+    By leveraging the Pyro4 library, this server ensures secure and high-performance communication
+    between various components of a distributed architecture.
+
+       With SecureGate's RPC server, businesses can effortlessly bridge the gap between their databases,
+       user interfaces, and messaging systems. Whether it's updating database records from a remote front-end
+       application, delivering real-time notifications through MQTT, or synchronizing data across multiple
+       databases, the SecureGate RPC server simplifies complex interactions into streamlined, efficient processes.
+
+       Powered by Pyro4, a trusted Python library, SecureGate's RPC server not only guarantees
+       the security of your data transfers but also ensures remarkable performance.
+       Pyro4's efficient communication protocols and advanced serialization techniques optimize the exchange
+       of data, allowing seamless integration without compromising speed or reliability.
+
+       In essence, SecureGate's RPC server, coupled with Pyro4, offers a secure, high-performance solution
+       for businesses seeking distributed systems integration. Whether you're managing databases,
+       user interfaces, or messaging services, SecureGate's RPC server provides a reliable foundation for
+       building interconnected, efficient, and secure applications
+    """
+
     def sign_in(self) -> typing.Dict[str, typing.Any]:
         ...
 
