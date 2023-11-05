db43288e4bea923707f27ef109ea707914c99a76
Isaac Santiago <eoisaacc@gmail.com>
2023-11-05 16:55:57 -0300

    feat: activate/deactivate device on button click

--- apps/emulator/src/main.py
+++ apps/emulator/src/main.py
@@ -1,3 +1,4 @@
+import json
 import time
 
 from apps.emulator.config.config import config
@@ -7,11 +8,35 @@ from packages.constants.mqtt_topics import MQTTTopic
 
 
 def on_activation_message(topic: str, payload: str):
-    print(topic, payload)
+    data = json.loads(payload)
+
+    if data["action"] == "ACTIVATE":
+        Log.info("Abrindo portão...")
+        time.sleep(3)
+        Log.success("Portão aberto com sucesso!")
+        return
+
+    if data["action"] == "DEACTIVATE":
+        Log.info("Fechando portão...")
+        time.sleep(3)
+        Log.success("Portão fechado com sucesso!")
+        return
+
+    Log.danger("Ação inválida!")
 
 
 def on_authorization_message(topic: str, payload: str):
     pass
+
+
+def rfid_read():
+    Log.break_()
+    Log.info("Aguardando RFID...")
+    rfid = input("=> ")
+    if not rfid or len(rfid) != 8:
+        Log.danger("Por favor, informe um RFID válido!")
+        return None
+    return rfid
 
 
 def _main():
@@ -28,6 +53,21 @@ def _main():
     mqtt.subscribe(MQTTTopic.AUTHORIZATION.value, on_authorization_message)
 
     mqtt.listen()
+
+    try:
+        while True:
+            Log.info("Aguardando RFID...")
+            rfid = input("=> ")
+            if not rfid or len(rfid) != 8:
+                Log.danger("Por favor, informe um RFID válido!")
+                continue
+
+            print(rfid)
+
+    except KeyboardInterrupt:
+        mqtt.stop()
+        Log.danger("Exiting...")
+        exit(0)
 
 
 _main()
